96cd093294ed787bf237c24fd5131c0244f99570
IBRAHIM Joreyj <ibrahimjoreyj93@gmail.com>
2026-08-20 14:11:25 +0300

    feat: implement les dossiers et les class

---
+++ src/main/java/org/example/examen_final_poo/exception/StockMovementNotFoundException.java
@@ -0,0 +1,4 @@
+package org.example.examen_final_poo.exception;
+
+public class StockMovementNotFoundException {
+}
---
+++ src/main/java/org/example/examen_final_poo/model/Product.java
@@ -0,0 +1,4 @@
+package org.example.examen_final_poo.model;
+
+public class Product {
+}
