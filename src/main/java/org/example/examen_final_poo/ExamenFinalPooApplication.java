43301147455260dca8d047b61ebca4ce823464fa
Adjmal <hei.adjmal.3@gmail.com>
2026-08-20 11:26:07 +0300

    Initialisation du projet Spring Boot

---
+++ src/main/java/org/example/examen_final_poo/ExamenFinalPooApplication.java
@@ -0,0 +1,13 @@
+package org.example.examen_final_poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalPooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalPooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/examen_final_poo/ExamenFinalPooApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examen_final_poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalPooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
